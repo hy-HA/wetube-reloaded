58270cd963941125b43f1e40fba98fa86b7788b8
hy-HA <73115727+hy-HA@users.noreply.github.com>
2021-12-28 22:33:03 +0900

    Extending Templates/Variables to Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,10 +2,7 @@
 //1.라우터 쓰기 > 2.라우터 만들기 >3.라우터 첫페이지 만들기-컨트롤러(o)
 export const trending = (req,res) => res.render("home");
 export const see = (req,res) => res.render("watch");
-export const edit = (req,res) => {
-    console.log(req.params);
-    return res.send("Edit");
-}
+export const edit = (req,res) => res.render("edit");
 export const search = (req,res) => res.send("Search");
 export const upload = (req,res) => res.send("Upload");
 export const deleteVideo = (req,res) => {
