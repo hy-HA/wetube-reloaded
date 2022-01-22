33e7117cab7f7cd1aed1d54069380bf39aed48c3
hy-HA <73115727+hy-HA@users.noreply.github.com>
2022-01-23 01:07:22 +0900

    Create Account_part2/3

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,12 +1,21 @@
 //1.라우터 쓰기 > 2.라우터 만들기 >3.라우터 첫페이지 만들기-컨트롤러(o)
-export const login = (req,res) => res.send("Login");
+import User from "../models/User";
+
 export const getJoin = (req,res) => res.render("join", {pageTitle: "Join"});
-export const postJoin = (req,res) => {
-    console.log(req.body);
-    res.end();
-}
+export const postJoin = async (req,res) => {
+    const { name, username, email, password, location } = req.body;
+    await User.create({
+        name,
+        username,
+        email,
+        password,
+        location,
+    });
+    return res.redirect("/login");
+};
 export const edit = (req,res) => res.send("Edit User");
 export const remove = (req,res) => res.send("Remove User");
 export const search = (req,res) => res.send("Search");
+export const login = (req,res) => res.send("Login");
 export const logout = (req,res) => res.send("Log out");
 export const see = (req,res) => res.send("See User");
