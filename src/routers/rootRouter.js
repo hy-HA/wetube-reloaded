7a4ef0b3d9486cc27a991575b9f64434699fdf3d
hy-HA <73115727+hy-HA@users.noreply.github.com>
2022-03-03 22:33:13 +0900

    Login part1

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { home, search } from "../controllers/videoController";
-import { getJoin,postJoin, login } from "../controllers/userController";
+import { getJoin,postJoin, getlogin } from "../controllers/userController";
 
 //1.라우터 쓰기 > 2.라우터 만들기(o) >3.라우터 첫페이지 만들기
 const rootRouter = express.Router();
@@ -8,7 +8,7 @@ const rootRouter = express.Router();
 //1.라우터 쓰기 > 2.라우터 만들기 >3.라우터 첫페이지 만들기-라우터(o)
 rootRouter.get("/", home);
 rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.get("/login", login);
+rootRouter.route("/login").get(getlogin);
 rootRouter.get("/search", search);
 
 //1.라우터 쓰기 > 2.라우터 만들기 >3.라우터 페이지 만들기>4.expoert하기
