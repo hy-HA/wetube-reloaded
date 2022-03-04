c9b926f03afb59dea0efc0c086d1f5514a782716
hy-HA <73115727+hy-HA@users.noreply.github.com>
2022-03-04 14:51:36 +0900

    ..

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 //1.라우터 쓰기 > 2.라우터 만들기 >3.라우터 첫페이지 만들기-컨트롤러(o)
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req,res) => res.render("join", {pageTitle: "Join"});
 export const postJoin = async (req,res) => {
@@ -40,18 +41,27 @@ export const getlogin = (req,res) =>
 
 export const postlogin = async (req,res) => {
     const {username, password} = req.body;
-    const exists = await User.exists({ username });
-    if (!exists) {
+    const pageTitle = "Login";
+    const user = await User.findOne({ username });
+    if (!user) {
         return res
             .status(400)
             .render("login", {
-                pageTitle: "Login",
+                pageTitle,
                 errorMessage: "An account with this username does not exists."
-            })
+            });
     }
-    // check if account exists
-    // check if password correct 
-    res.end();
+    const ok = await bcrypt.compare(password, user.password);
+    if (!ok) {
+        return res
+            .status(400)
+            .render("login", {
+                pageTitle,
+                errorMessage: "Wrong password.",
+            });
+    }
+    console.log("Log User IN! COMING SOON!")
+    return res.redirect("/");
     };
 
 export const edit = (req,res) => res.send("Edit User");
