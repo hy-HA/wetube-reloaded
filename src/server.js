e0812c9ab9c39e95ee0017025a73ad83b31eee98
hy-HA <73115727+hy-HA@users.noreply.github.com>
2022-03-04 15:59:29 +0900

    sessions and cookies part1

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 //const express = require("express"); 코드의 최신버전 
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -19,6 +20,15 @@ app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 //1.라우터 쓰기(o) > 2.라우터 만들기 >3.라우터 첫페이지 만들기
 app.use(express.urlencoded({extended:true}));
+
+app.use(
+    session({
+        secret: "Hello!",
+        resave: true,
+        saveUninitialized: true,
+    })
+)
+
 app.use("/",rootRouter);
 app.use("/videos",videoRouter);
 app.use("/users",userRouter);
