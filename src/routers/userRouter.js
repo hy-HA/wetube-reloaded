6020a826678ea69afe651fe5705aa62e875b1d50
hy-HA <73115727+hy-HA@users.noreply.github.com>
2022-03-07 04:35:03 +0900

    Github Login part1/2

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {edit, remove, logout, see} from "../controllers/userController";
+import {edit, remove, logout, see, startGithubLogin, finishGithubLogin} from "../controllers/userController";
 
 
 //1.라우터 쓰기 > 2.라우터 만들기(o) >3.라우터 페이지 만들기
@@ -8,6 +8,8 @@ const userRouter = express.Router();
 userRouter.get("/logout",logout);
 userRouter.get("/edit",edit);
 userRouter.get("/remove", remove);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get(":id",see);
 
 //1.라우터 쓰기 > 2.라우터 만들기(o) >3.라우터 페이지 만들기>4.expoert하기
