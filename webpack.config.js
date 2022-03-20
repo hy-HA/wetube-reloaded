06c74241536ed7d4c459c4e28829ae9e40437892
hy-HA <73115727+hy-HA@users.noreply.github.com>
2022-03-20 17:51:38 +0900

    webpack configuration part2 error

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
+//path import
+//const path = require("path");
+//js폴더까지 찾아짐
+//console.log(path.resolve(__dirname,"assets","js"));
+//wetube폴더까지 찾아짐
+//console.log(__dirname);
+
 module.export = {
     //파일의 경로를 입력
     entry : "./src/client/js/main.js",
     output : {
         //파일 이름 지정
         filename: "main.js",
-        //파일이 저장될 위치 지정
-        path: "./assets/js",
-    }
+        //파일이 저장될 위치 지정(절대경로로 지정해야함)
+        path: path.resolve(__dirname, "assets", "js"),
+    },
 };
