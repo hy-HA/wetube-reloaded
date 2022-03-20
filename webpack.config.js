d6dace45fe2cbba9d184204cc3f37867242dfe2c
hy-HA <73115727+hy-HA@users.noreply.github.com>
2022-03-20 17:14:28 +0900

    webpack configuration part1

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.export = {
+    //파일의 경로를 입력
+    entry : "./src/client/js/main.js",
+    output : {
+        //파일 이름 지정
+        filename: "main.js",
+        //파일이 저장될 위치 지정
+        path: "./assets/js",
+    }
+};
