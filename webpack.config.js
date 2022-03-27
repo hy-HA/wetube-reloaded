89832653fcef528bbc2536385afa140b8f29fa3f
hy-HA <73115727+hy-HA@users.noreply.github.com>
2022-03-27 20:47:29 +0900

    webpack configuration part2

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,34 @@
 //path import
-//const path = require("path");
+const path = require("path");
+
 //js폴더까지 찾아짐
 //console.log(path.resolve(__dirname,"assets","js"));
+
 //wetube폴더까지 찾아짐
 //console.log(__dirname);
 
-module.export = {
+module.exports = {
     //파일의 경로를 입력
     entry : "./src/client/js/main.js",
+    mode: 'development',
     output : {
         //파일 이름 지정
         filename: "main.js",
         //파일이 저장될 위치 지정(절대경로로 지정해야함)
         path: path.resolve(__dirname, "assets", "js"),
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [["@babel/preset-env", {targets: "defaults"}]],
+                    },
+                },
+            },
+        ],
+    },
+    
 };
