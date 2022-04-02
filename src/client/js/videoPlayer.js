b349ca7491fd698af9d1368ecca83ff3575c5913
hy-HA <73115727+hy-HA@users.noreply.github.com>
2022-04-03 00:51:04 +0900

    play pause

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,28 @@
-console.log("video Player!");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+//제대로 자바스크립트에 불러오는지 확인. console.log(play, mute, time, volume);
+
+//비디오를 멈추고 재생하는 기능
+const handlePlayClick = (e) => {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+};
+
+const handleMute = (e) => {
+
+};
+
+//비디오 멈춤/재생시 텍스트를 변경하는 기능
+const handlePause = () => (playBtn.innerText = "Play");
+const handlePlay = () => (playBtn.innerText = "Pause");
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
